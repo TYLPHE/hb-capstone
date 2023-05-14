ddcb50332631a1c3809d8f57b58e6be26605dc71
TYLPHE <tylphet@gmail.com>
2023-05-14 16:27:17 -0600

    Working on register page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,28 @@
 export default function Register() {
+  function handleUsername() {
+    return
+  }
+  function handlePassword() {
+    return 
+  }
+  function handleSubmit() {
+    return 
+  }
+  
   return (
     <div>
-      Registration Page
+      <h1>Create your account</h1>
+      <form>
+      <div>
+          <label htmlFor="username">Sign in with account name</label>
+          <input type="text" id="username" onChange={(e) => handleUsername(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" onChange={(e) => handlePassword(e.target.value)} />
+        </div>
+        <button onClick={(e) => handleSubmit(e)}>Create Account</button>
+      </form>
     </div>
   )
 }
