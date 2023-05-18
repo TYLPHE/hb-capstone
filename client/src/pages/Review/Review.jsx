890c1c2a5e3c330751de7ff6bfbd6e94df7e335b
TYLPHE <tylphet@gmail.com>
2023-05-18 02:07:33 -0600

    Working on review page css

--- client/src/pages/Review/Review.jsx
+++ client/src/pages/Review/Review.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 import MDEditor from "@uiw/react-md-editor";
 import Header from '../../common/Header/Header'
+import './Review.css';
 
 export default function Review() {
   const {
@@ -25,25 +26,33 @@ export default function Review() {
   return (
     <div>
       <Header />
-      <h1>{ game }</h1>
-        <Link to={`/games/${ game_id }/${ game }`} >
-          <img src={header_image} alt="header of game" />
-        </Link>
-      { score && <MyScore /> }
-      <div>
-        <Link to={`/review/edit/${review_id}`}>
-          <button>Edit Review</button>
-        </Link>
-      </div>
-        <MDEditor.Markdown source={review} />
-
-      <div>
-        <Link 
-          to={`/review/delete/${review_id}`} 
-          state={{ game, game_id, header_image, review_id }}
-        >
-          <button>Delete review and remove from library</button>
-        </Link>
+      <div className="main-view">
+        <div className="review-header">
+          <Link to={`/games/${ game_id }/${ game }`} >
+            <img 
+              src={header_image} 
+              alt="header of game" 
+              className="review-image" 
+            />
+          </Link>
+          <div className="review-data">
+            { score && <MyScore /> }
+            <div>
+              <Link to={`/review/edit/${review_id}`}>
+                <button className="review-button">Edit Review</button>
+              </Link>
+            </div>
+            <div>
+              <Link 
+                to={`/review/delete/${review_id}`} 
+                state={{ game, game_id, header_image, review_id }}
+              >
+                <button className="review-button">Delete review and remove from library</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+          <MDEditor.Markdown source={review} />
       </div>
     </div>
   );
